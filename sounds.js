6f37b2b73f456d1b3ab22ffcc64dd201b9b2525f
TMallah123 <77752252+TMallah123@users.noreply.github.com>
2024-04-16 19:37:24 -0400

    Add files via upload

--- sounds.js
+++ sounds.js
@@ -1,3 +1,12 @@
 
     const audio1 = new Audio();
     audio1.src="Meow1.mp3";
+    const audio2 = new Audio();
+    audio2.src="Bieber.mp3";
+    const audio3 = new Audio();
+    audio3.src="Gravity.mp3";
+    const audio4 = new Audio();
+    audio4.src="highground.mp3";
+    const audio5 = new Audio();
+    audio5.src="Uncivilized.mp3";
+
